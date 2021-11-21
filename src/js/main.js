40b9996c5aa36f85fc9abd5f455a01656b4dc47e
Adewale David <adewaledavedev@gmail.com>
2021-11-21 11:10:12 +0100

    Responsiveness changed

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,13 @@ $("document").ready(function () {
 	$("button:first").click(function () {
 		$("body").removeClass("darktheme")
 		$(".hero-img").attr("src", "img/Hero Image-light.png")
+		$(".logo").attr("src", "img/Remix Logo light Mode.svg")
 	})
 
 	$("button:last").click(function () {
 		$("body").addClass("darktheme")
 		$(".hero-img").attr("src", "img/Hero Image-dark.png")
+		$(".logo").attr("src", "img/Remix-logo light mode.svg")
 	})
 
 })
