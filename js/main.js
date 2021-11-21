561a9040fa2ea9d6a361db7e130a2845506301e8
Adewale David <adewaledavedev@gmail.com>
2021-11-21 09:22:34 +0100

    A demo figma plugin landing page with dark mode and light mode

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$("document").ready(function(){
+
+$("button:first").click(function(){
+	$("body").removeClass("darktheme")
+	$(".hero-img").attr("src", "../img/Hero Image-light.png")
+})
+
+$("button:last").click(function(){
+	$("body").addClass("darktheme")
+	$(".hero-img").attr("src", "../img/Hero Image-dark.png")
+})
+
+})
